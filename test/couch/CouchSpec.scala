006940046064fd9e104f32efb5c6bba2b71a0df7
Mike Bush <mpbush@gmail.com>
2013-11-15 17:37:54 +0100

    Add the rest of the tests for adding and removing databases

--- test/couch/CouchSpec.scala
+++ test/couch/CouchSpec.scala
@@ -8,16 +8,49 @@ import models._
 
 class CouchSpec extends FlatSpec with ShouldMatchers with GivenWhenThen with MockFactory {
 	val dbUrl = "http://localhost:5984/"
-	val testDb = "scala-couch-test"
+	val testDbName = "scala-couch-test"
 
 	"Couch.addDb" should "succeed if that db has just been removed" in {
 		Given("a couch instance")
 		val couch = Couch.url(dbUrl)
 		When("test db is removed")
-		Await.result(couch.removeDb(testDb), 1.second)
-		And("test db is added again")
-		val result = Await.result(couch.addDb(testDb), 1.second)
+		Await.result(couch.removeDb(testDbName), 1.second)
+		And("test db is added")
+		val result = Await.result(couch.addDb(testDbName), 1.second)
 		Then("The response should be successful")
 		result should be (true)
+	}
+
+	it should "fail if that db already exists" in {
+		Given("a couch instance")
+		val couch = Couch.url(dbUrl)
+		When("test db is added")
+		Await.result(couch.addDb(testDbName), 1.second)
+		And("test db is added again")
+		val result = Await.result(couch.addDb(testDbName), 1.second)
+		Then("The response should be failure")
+		result should be (false)
+	}
+
+	"Couch.removeDb" should "succeed if that db has just been added" in {
+		Given("a couch instance")
+		val couch = Couch.url(dbUrl)
+		When("test db is added")
+		Await.result(couch.addDb(testDbName), 1.second)
+		And("test db is removed")
+		val result = Await.result(couch.removeDb(testDbName), 1.second)
+		Then("The response should be successful")
+		result should be (true)
+	}
+
+	it should "fail if that db doesn't exist" in {
+		Given("a couch instance")
+		val couch = Couch.url(dbUrl)
+		When("test db is not present")
+		Await.result(couch.removeDb(testDbName), 1.second)
+		And("test db is removed again")
+		val result = Await.result(couch.removeDb(testDbName), 1.second)
+		Then("The response should be failure")
+		result should be (false)
 	}
 }
