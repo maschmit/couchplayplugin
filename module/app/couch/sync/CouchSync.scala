71eb473c20baef19f3e9a01f0e44ad18e88e818a
Mike Bush <mpbush@gmail.com>
2014-01-13 19:38:47 +0100

    Also be able to check for matches without making any change

--- module/app/couch/sync/CouchSync.scala
+++ module/app/couch/sync/CouchSync.scala
@@ -25,6 +25,13 @@ object CouchSync {
   	  	case Some(doc) if doc.body.equals(json) => Future.successful(doc.head)
   	  	case Some(doc) => ptr.rev(doc.head.rev).replace(json)
   	  }
+
+    def check(ptr: DocumentPointer): Future[MatchResult] = 
+      ptr.getOpt().map {
+        case Some(doc) if doc.body.equals(json) => Match()
+        case None => NoMatch()
+        case Some(doc) => NoMatch()
+      }
   }
 
   class DirectoryCouchDocumentSetSync(root: Option[String], dir: File) extends CouchDocumentSetSync {
@@ -36,6 +43,14 @@ object CouchSync {
           new DirectoryCouchDocumentSetSync(Some(subRoot(subdir.getName)), subdir).to(db)
   	  }
 
+    def check(db: CouchDatabase): Future[MatchResult] = 
+      Future.reduce(dir.listFiles.map {
+        case file if file.isFile && file.getName().endsWith(".json") =>
+          new JsonCouchDocumentSync(getJson(file)).check(db.doc(jsonFileName(file.getName)))
+        case subdir if subdir.isDirectory =>
+          new DirectoryCouchDocumentSetSync(Some(subRoot(subdir.getName)), subdir).check(db)
+      }) (MatchResult.reduce _)
+
     private def jsonFileName(name: String) = root.map(_ + "/").getOrElse("") + name.substring(0,name.length-5)
     private def subRoot(name: String) = root.map(_ + "/").getOrElse("") + name
   }
@@ -46,8 +61,22 @@ object CouchSync {
 
 trait CouchDocumentSync {
   def to(ptr: DocumentPointer): Future[DocumentHeader]
+  def check(ptr: DocumentPointer): Future[MatchResult]
 }
 
 trait CouchDocumentSetSync {
   def to(db: CouchDatabase): Seq[Future[Any]]
+  def check(db: CouchDatabase): Future[MatchResult]
 }
+
+object MatchResult {
+  def reduce(_1: MatchResult, _2: MatchResult): MatchResult =
+    if (_1.isInstanceOf[NoMatch] || _2.isInstanceOf[NoMatch])
+      NoMatch()
+    else
+      Match()
+}
+
+sealed abstract class MatchResult
+case class Match() extends MatchResult
+case class NoMatch() extends MatchResult
--- module/test/couch/CouchSyncSpec.scala
+++ module/test/couch/CouchSyncSpec.scala
@@ -1,7 +1,7 @@
 package couch
 
 import sync.CouchDesignDocument
-import sync.CouchSync
+import sync._
 
 import org.scalatest._
 import org.scalamock.scalatest.MockFactory
@@ -51,7 +51,7 @@ class CouchSyncSpec extends FlatSpec with ShouldMatchers with GivenWhenThen with
   it should "replace an existing doc if it has different contents" in {
     Given("an existing doc")
     Await.result(docPtr.create(mapReduceDesign), 1.second)
-    When("an identical doc is synced")
+    When("a differing doc is synced")
     Await.result(CouchSync(mapDesign).to(docPtr), 1.second)
     Then("the doc should contain the new version")
     val document = Await.result(docPtr.get(), 1.second)
@@ -78,5 +78,47 @@ class CouchSyncSpec extends FlatSpec with ShouldMatchers with GivenWhenThen with
     doc1.body should be (mapDesign)
     val doc2 = Await.result(testDb.get("_design/mapReduceDoc"), 1.second)
     doc2.body should be (mapReduceDesign)
+  }
+
+  "CouchSync(Json).check(DocumentPointer)" should "not match if it exists locally and not in the db" in {
+    When("a new doc is checked")
+    val result = Await.result(CouchSync(mapDesign).check(docPtr), 1.second)
+    Then("the result should be not matched")
+    result should be (NoMatch())
+  }
+
+  it should "match if an identical doc already exists" in {
+    Given("an existing doc")
+    val creationResult = Await.result(docPtr.create(mapDesign), 1.second)
+    When("an identical doc is checked")
+    val result = Await.result(CouchSync(mapDesign).check(docPtr), 1.second)
+    Then("the result should be a match")
+    result should be (Match())
+  }
+
+  it should "not match if an existing doc has different contents" in {
+    Given("an existing doc")
+    Await.result(docPtr.create(mapReduceDesign), 1.second)
+    When("a differing doc is checked")
+    val result = Await.result(CouchSync(mapDesign).check(docPtr), 1.second)
+    Then("the result should not be a match")
+    result should be (NoMatch())
+  }
+
+  "CouchSync(directoryPath).check(Database)" should "not match if they exist locally and not in the db" in {
+    When("a new dir is checked")
+    val result = Await.result(CouchSync(new File("test/couch/testfiles")).check(testDb), 1.second)
+    Then("the result should be not matched")
+    result should be (NoMatch())
+  }
+
+  it should "match if the files have already been synced" in {
+    Given("the dir is already synced")
+    val syncFutures = CouchSync(new File("test/couch/testfiles")).to(testDb)
+    syncFutures.foreach(Await.result(_, 1.second))
+    When("the dir is checked")
+    val result = Await.result(CouchSync(new File("test/couch/testfiles")).check(testDb), 1.second)
+    Then("the result should be matched")
+    result should be (Match())
   }
 }
