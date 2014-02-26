c1f73143b8cd8612b90586f717104fac4eaafdf9
Mike Bush <mpbush@gmail.com>
2014-02-26 14:19:48 +0100

    Fix whitespace in repository

--- samples/time-counter/app/repositories/CounterRepositoryComponent.scala
+++ samples/time-counter/app/repositories/CounterRepositoryComponent.scala
@@ -22,49 +22,45 @@ trait CounterRepositoryComponent {
   }
   
   class CouchCounterRepository extends CounterRepository {
-	import play.api.libs.json._
-	import play.api.libs.functional.syntax._
+    import play.api.libs.json._
+    import play.api.libs.functional.syntax._
 
-	val couchDb = Couch.load.db("default")
+    val couchDb = Couch.load.db("default")
 
-	implicit val counterWrites = new Writes[Counter] {
-	  def writes(c: Counter): JsValue = {
-	  	Json.obj(
-	  		"type" -> "counter",
-	  		"name" -> c.name
-	  		)
-	  }
-	}
+    implicit val counterWrites = new Writes[Counter] {
+      def writes(c: Counter): JsValue = Json.obj(
+          "type" -> "counter",
+          "name" -> c.name
+        )
+    }
 
-	implicit val incrementWrites = new Writes[CounterIncrement] {
-	  def writes(c: CounterIncrement): JsValue = {
-	  	Json.obj(
-	  		"type" -> "increment",
-	  		"counterId" -> c.counterId,
-	  		"minutes" -> c.minutes
-	  	  )
-	  }
-	}
+    implicit val incrementWrites = new Writes[CounterIncrement] {
+      def writes(c: CounterIncrement): JsValue = Json.obj(
+          "type" -> "increment",
+          "counterId" -> c.counterId,
+          "minutes" -> c.minutes
+        )
+    }
   
-	import play.api.libs.concurrent.Execution.Implicits.defaultContext
+    import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
     def counters: Future[Seq[CounterWithAggregate]] = 
       couchDb.design("counters").view("counters").grouped.get().map(_.rows.map {
-      	case ReduceViewElement(key, value) => for {
-      	  k <- key.asOpt[String]
-      	  name <- (value \ "name").asOpt[String]
-      	  minutes <- (value \ "minutes").asOpt[Int]
-      	} yield new CounterWithAggregate(k, Counter(name), TimeCounter(minutes))
+          case ReduceViewElement(key, value) => for {
+            k <- key.asOpt[String]
+            name <- (value \ "name").asOpt[String]
+            minutes <- (value \ "minutes").asOpt[Int]
+          } yield new CounterWithAggregate(k, Counter(name), TimeCounter(minutes))
       }.collect { case Some(v) => v })
 
-	def add(newCounter: Counter): Future[CounterId] = 
-	  couchDb.create(Json.toJson(newCounter)).map(created =>
-	  	  CounterId(created.id))
+    def add(newCounter: Counter): Future[CounterId] = 
+      couchDb.create(Json.toJson(newCounter)).map(created =>
+            CounterId(created.id))
 
-  	def increment(increment: CounterIncrement) = 
-  	  couchDb.create(Json.toJson(increment)).map(r => ())
+      def increment(increment: CounterIncrement) = 
+        couchDb.create(Json.toJson(increment)).map(r => ())
 
-  	def increment(id: CounterId, time: Time) = 
-  	  increment(CounterIncrement(id.id, time.minutes))
+      def increment(id: CounterId, time: Time) = 
+        increment(CounterIncrement(id.id, time.minutes))
   }
 }
