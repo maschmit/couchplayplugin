46d118b11f9b78aa9c747dcfae1c43a55e021f57
Mike Bush <mpbush@gmail.com>
2014-03-03 12:40:44 +0100

    Add some documentation to CouchDesign

--- module/app/couch/CouchDesign.scala
+++ module/app/couch/CouchDesign.scala
@@ -11,9 +11,16 @@ import play.api.libs.ws._    // could make some wrapper to avoid using ws & json
 import play.api.libs.concurrent.Execution.Implicits.defaultContext // TODO : implicitly pass in
 
 
+/** Starts building queries against a design document */
 class CouchDesign(private val designRequestGen: RequestGenerator) {
   lazy val url = designRequestGen(Nil).url
 
+  /** Start building a view query
+    *
+    * {{{
+    * couchDatabase.design("test").view("counters").grouped.reduced.get()
+    * }}}
+    */
   def view(name: String) = new ViewQueryBuilder[ViewResult](designRequestGen("_view" :: name :: Nil))
 }
 
@@ -22,21 +29,25 @@ class ViewQueryBuilder[T <: ViewResult](val designRequest: WS.WSRequestHolder,
 
   implicit def url: String = request.url
       
-  def request = designRequest.withQueryString(Seq(("group", group), ("reduce", reduce))
+  private def request = designRequest.withQueryString(Seq(("group", group), ("reduce", reduce))
     .collect { case (name, Some(value)) => (name, value.toString) }: _*)
 
+  /** Groups the reduction by key for a reduce query (fails for a view query) */
   def grouped = new ViewQueryBuilder[T](designRequest, Some(true), reduce)
   
-  /** Sets the reduce=false parameter on the view request - this should mean
-    * that a Future[MapViewResult] is returned
+  /** Sets the reduce=false parameter on the view request.
+    *
+    * A Future[MapViewResult] is returned when get() is called
     */
   def notReduced = new ViewQueryBuilder[MapViewResult](designRequest, group, Some(false))
 
-  /** Sets the reduce=true parameter on the view request - this should mean
-    * that either a Future[ReduceViewResult] is returned or a CouchError is thrown
+  /** Sets the reduce=true parameter on the view request.
+    *
+    * Either a Future[ReduceViewResult] is returned or a CouchError is thrown
     */
   def reduced = new ViewQueryBuilder[ReduceViewResult](designRequest, group, Some(true))
 
+  /** Perform view query asyncronously */
   def get(): Future[T] = 
     request.get().map( response => response.status match {
         case 200 => response.json.as[ViewResult].asInstanceOf[T]
