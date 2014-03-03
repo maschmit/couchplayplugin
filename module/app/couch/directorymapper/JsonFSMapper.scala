6fcf85e0a4634597d2b97a4785287ec62e6a1801
Mike Bush <mpbush@gmail.com>
2014-03-03 19:28:28 +0100

    Document JsonFSMapper

--- module/app/couch/directorymapper/JsonFSMapper.scala
+++ module/app/couch/directorymapper/JsonFSMapper.scala
@@ -4,6 +4,16 @@ import java.io.File
 
 import play.api.libs.json._
 
+/** Maps a file or directory to a json value
+  *
+  * A file with extension .json is parsed to a JsValue.
+  *
+  * A file with any other extension is read to a JsString.
+  *
+  * A directory is loaded as a JsObject with fields for each of it's children where the
+  * key is the base of the filename and the value is the value loaded for the file using
+  * these 3 rules.
+  */
 object JsonFSMapper {
 	def load(f: File): JsValue = f match {
 	  case file if file.isFile && hasJsonExtension(file) => Json.parse(io.Source.fromFile(file).mkString)
